9055424047f535310aca456221835cd295c77234
zonestore44552-web <zonestore44552@gmail.com>
2026-06-07 16:33:38 -0300

    fix: add trust proxy for Render reverse proxy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const rateLimit = require('express-rate-limit');
 const { spawn } = require('child_process');
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cors());
 
